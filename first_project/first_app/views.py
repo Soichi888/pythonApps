9ad1c51a489deb85fce2da091fc664a9c25ff52d
Soichi Takahashi <soichitakahashi@Soichis-MacBook-Air.local>
2018-09-18 22:42:06 -0700

    Added Models

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 
 def index(request):
-	my_dict = {'insert_me': "Some update"}
-	return render(request,'first_app/index.html', context=my_dict)
+	webpages_list = AccessRecord.objects.order_by('date')
+	data_dict = {'access_records':webpages_list}
+	return render(request,'first_app/index.html', context=data_dict)
 
 
 def help(request):
