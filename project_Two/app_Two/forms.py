11b17d952fb89c7167f07de2190eb7fa226be86c
Soichi Takahashi <soichitakahashi@Soichis-MacBook-Air.local>
2018-09-24 16:17:07 -0700

    added Udemy django class level four(Forms) and level five(Login,Logout)

--- project_Two/app_Two/forms.py
+++ project_Two/app_Two/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from app_Two.models import User
 
+
 # class FormUser(forms.Form):
 #     first_name = forms.CharField()
 #     last_name = forms.CharField()
 #     email = forms.EmailField()
+
 class NewUserForm(forms.ModelForm):
     class Meta():
         model = User
